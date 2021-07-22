330f3bbd14f401b343cb7e080062bca68b41523c
easy1090 <752355956@qq.com>
2021-07-22 11:10:44 +0800

    fix: publish 0.1.0

--- src/util/getPackageVersion.ts
+++ src/util/getPackageVersion.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const requestFn = (url: string) => {
+const requestFn = (url: string, range: string) => {
+  const registry = `https://registry.npm.taobao.org`; // TODO区分淘宝源与npm源
   return axios({
     method: 'get',
-    url,
+    url: `${registry}/${encodeURIComponent(url).replace(/^%40/, '@')}/${range}`,
     responseType: 'json',
     timeout: 30000,
   })
